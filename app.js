fd855f68aec6733e436d67e9ae4d1b387c3939ae
TM-WebD3V <yasuounforgiven779@gmail.com>
2024-02-25 18:19:23 +0100

    finaly

---
+++ app.js
@@ -0,0 +1,55 @@
+const images = [
+  "./img/1.jpg",
+  "./img/2.jpg",
+  "./img/3.jpg",
+  "./img/4.jpg",
+  "./img/5.jpg",
+  "./img/6.jpg",
+  "./img/7.jpg",
+  "./img/8.jpg",
+  "./img/9.jpg",
+  "./img/10.jpg",
+  "./img/11.jpg",
+  "./img/12.jpg",
+  "./img/13.jpg",
+  "./img/14.jpg",
+  "./img/15.jpg",
+  "./img/16.jpg",
+  "./img/17.jpg",
+  "./img/18.jpg",
+  "./img/19.jpg",
+  "./img/20.jpg",
+  "./img/21.jpg",
+  "./img/22.jpg",
+  "./img/23.jpg",
+  "./img/24.jpg",
+  "./img/25.jpg",
+  "./img/26.jpg",
+  "./img/27.jpg",
+  "./img/28.jpg",
+  "./img/29.jpg",
+  "./img/30.jpg",
+  "./img/31.jpg",
+  "./img/32.jpg",
+  "./img/cesma.jpg",
+  "./img/cesma2.jpg",
+  "./img/izvor.png",
+  "./img/river.jpg",
+  "./img/river2.jpg",
+  "./img/sheep7.jpg",
+  "./img/homes.jpg",
+  "./img/horizon.jpg",
+  "./img/horizon2.jpg",
+  "./img/horizon3.jpg",
+  "./img/horizon4.jpg",
+  "./img/horizon5.jpg",
+  "./img/horizon6.jpg",
+];
+
+const container = document.querySelector(".grid-container");
+images.forEach((image) => {
+  const img = document.createElement("img");
+  img.src = image;
+  img.alt = "images of nature,trees, rivers and other things";
+  container.appendChild(img);
+});
